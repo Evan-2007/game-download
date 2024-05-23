59bc1a48287cae0f908cf7e172d86a7bd5e621e0
Evan Cooper <65788884+Evan-2007@users.noreply.github.com>
2024-05-23 09:37:27 -0700

    fixed speling mistake and centerd text

--- src/app/uploading/page.tsx
+++ src/app/uploading/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
-    <div className="flex justify-center flex-col items-center mt-48">
-      <p className="text-3xl mt-3">Game is currently uploading please come pack later</p>
+    <div className="flex justify-center flex-col items-center mt-48 text-center">
+      <p className="text-3xl mt-3">Game is currently uploading please come back later</p>
     </div>
   )
 }
