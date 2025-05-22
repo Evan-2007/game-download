352b1de328bbcdf523e2b6b2c54b4698933e0c35
Evan Cooper <65788884+Evan-2007@users.noreply.github.com>
2025-05-22 15:33:21 -0700

    added new games and shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "next-themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,9 +16,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <title>GECS Games</title>
-      <body className={inter.className}>{children}</body>
+      <body className={"w-screen h-screen bg-black" + inter.className}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,15 @@
 import { platform } from "os"
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardFooter,
+  CardDescription,
+} from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+import { Separator } from "@/components/ui/separator"
+
 
 interface Games {
   name: string
@@ -15,38 +26,40 @@ const games: Years[] = [
   {
     year: "2025",
     games: [
-
+      {
+        name: 'Elemental Amulet',
+        platform: ['Windows'],
+        links: [ '2025/Elemental Amulet.zip']
+      },
+      {
+        name: 'Incognito',
+        platform: ['Windows'],
+        links: [ '2025/Incognito-Windows.zip']
+      },
+      {
+        name: 'Ossify',
+        platform: ['Windows'],
+        links: [ '2025/OssifyGame.zip']
+      },
+      {
+        name: 'Zoomies',
+        platform: ['Windows'],
+        links: [ '2025/Zoomies.zip']
+      },
     ]
   },
   {
     year: "2024", 
     games:[
-      {
-        name: 'Escape',
-        platform: ['Windows'],
-        links: [ '2025/Elemental Amulet.zip']
-      },
-      {
-        name: 'Eye Spy',
-        platform: ['Windows'],
-        links: [ 'Eye Spy.zip', 'uploading']
-      },
-      {
-        name: 'Dungeon Delve',
-        platform: ['Windows'],
-        links: [ 'DungeonDelve.zip']
-      }
-    ]
-    games: [
         {
           name: 'Escape',
           platform: ['Windows'],
-          links: [ 'escape.zip', 'uploading']
+          links: [ 'escape.zip']
         },
         {
           name: 'Eye Spy',
           platform: ['Windows'],
-          links: [ 'Eye Spy.zip', 'uploading']
+          links: [ 'Eye Spy.zip']
         },
         {
           name: 'Dungeon Delve',
@@ -56,51 +69,57 @@ const games: Years[] = [
         {
           name: 'Race Rave Ice Edition',
           platform: ['Windows'],
-          links: [ 'RaceRaveIceEdition.zip', 'uploading']
+          links: [ 'RaceRaveIceEdition.zip']
         },
         {
           name: 'Root Beer Runner',
           platform: ['Windows'],
-          links: [ 'RootBeerRunner.zip', 'uploading']
-        },
+          links: [ 'RootBeerRunner.zip']
+        },]
+      }
     ]
-  },
-]
 
 
 export default function page() {
   return (
-    <div className="flex justify-center flex-col items-center mb-24">
-      <p className="text-4xl mt-3 mr-10">GECS Games</p>
-      <div className="flex flex-row flex-wrap justify-center items-center">
-        {games.map((game, index) => (
-          <GameCard key={index} game={game} />
-        ))}
-      </div>
+    <div className="flex justify-center flex-col items-center mb-24 bg-background">
+      {
+        games.map((year, index) => (
+          <div key={index} className="mt-10">
+            <h1 className="text-3xl text-center text-white">{year.year}</h1>
+            <div className="flex flex-wrap justify-center mt-5 space-x-5">
+              {year.games.map((game, index) => (
+                <GameCard key={index} game={game} />
+              ))}
+            </div>
+          </div>
+        ))
+      }
     </div>
   )
 }
 
 function GameCard({ game }: { game: Games }) {
   return (
-    <div className="flex flex-col items-center justify-center bg-gray-950 w-64 mt-5 h-36 rounded-2xl mr-10">
-      <h1 className="text-3xl text-wrap text-center">{game.name}</h1>
-      <h1 className="text-1xl mt-4">Downloads:</h1>
-      <div className="flex flex-row items-center justify-between mt-2">
-        {game.links.map((link, index) => (
-          <>
-            <a
-              key={index}
-              href={link === 'uploading' ? "/uploading" : `https://assets.evanc.dev/games/gecs/${link}`}
-              target="_blank"
-              className="text-center mr-2 ml-2 hover:underline text-blue-500"
-            >
-              {game.platform[index]}
-            </a>
-            {index !== game.links.length - 1 && <h1 className="text-1xl"></h1>}
-          </>
-        ))}
-      </div>
-    </div>
+    <Card className="w-64 bg-card text-card-foreground">
+      <CardHeader>
+        <CardTitle>{game.name}</CardTitle>
+        <CardDescription>
+          {game.platform.map((platform, index) => (
+            <span key={index}>{platform}{index < game.platform.length - 1 ? ', ' : ''}</span>
+          ))}
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Separator />
+        <div className="flex flex-col items-center">
+          {game.links.map((link, index) => (
+            <Button key={index} variant="outline" className="mt-2">
+              <a href={`https://assets.evanc.dev/games/gecs/${link}`} download>{game.platform[index]}</a>
+            </Button>
+          ))}
+        </div>
+      </CardContent>
+    </Card>
   )
 }
